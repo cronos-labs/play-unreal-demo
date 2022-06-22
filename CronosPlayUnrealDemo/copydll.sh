508ac2a172a8e905fa7b6dadf1ea380f135718ef
Jongwhan Lee <51560997+leejw51crypto@users.noreply.github.com>
2022-06-22 20:04:05 +0900

    Problem: demo not updated (fix #63) (#6)
    don't use sudo
    
    change document
    
    fix crash

--- CronosPlayUnrealDemo/copydll.sh
+++ CronosPlayUnrealDemo/copydll.sh
@@ -1,3 +1,6 @@
+install_name_tool -id @rpath/libplay_cpp_sdk.dylib ./Plugins/CronosPlayUnreal/Source/CronosPlayUnreal/Private/cronosplay/lib/Mac/libplay_cpp_sdk.dylib
+# check
+otool -L ./Plugins/CronosPlayUnreal/Source/CronosPlayUnreal/Private/cronosplay/lib/Mac/libplay_cpp_sdk.dylib
 cp ./Plugins/CronosPlayUnreal/Source/CronosPlayUnreal/Private/cronosplay/lib/Mac/libplay_cpp_sdk.dylib  ./Binaries/Mac
 cp ./Plugins/CronosPlayUnreal/Source/CronosPlayUnreal/Private/cronosplay/lib/Win64/play_cpp_sdk.dll  ./Binaries/Win64
 
