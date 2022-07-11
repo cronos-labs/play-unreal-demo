c3d3cec571a175d46411cb1051d9982ea9659c2d
Jongwhan Lee <51560997+leejw51crypto@users.noreply.github.com>
2022-07-11 16:06:21 +0900

    Problem: missing api for tokenuri download and texture generation (fix #75) (#7)
    update plugin
    
    update plugin
    
    change repo
    
    update plugin

--- CronosPlayUnrealDemo/Source/CronosPlayUnrealDemo/CronosPlayUnrealDemo.Build.cs
+++ CronosPlayUnrealDemo/Source/CronosPlayUnrealDemo/CronosPlayUnrealDemo.Build.cs
@@ -1,12 +1,12 @@
 
 using UnrealBuildTool;
 
-public class CronosPlayUnrealDemo : ModuleRules
-{
-	public CronosPlayUnrealDemo(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+public class CronosPlayUnrealDemo : ModuleRules {
+  public CronosPlayUnrealDemo(ReadOnlyTargetRules Target) : base(Target) {
+    PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
-	}
+    PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject",
+                                                        "Engine", "InputCore",
+                                                        "HeadMountedDisplay" });
+  }
 }
