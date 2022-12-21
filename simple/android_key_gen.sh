0c2e4cc8a0798f740a03332b50051ec599af5df4
Damon Chen <damon.chen@crypto.com>
2022-12-21 14:33:07 +0800

    Update android_key_gen.sh to support non macOS

--- simple/android_key_gen.sh
+++ simple/android_key_gen.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+if [[ $(uname) == "Darwin" ]]; then
 /Applications/Android\ Studio.app/Contents/jre/Contents/Home/bin/keytool \
     -genkey -v -keystore key.keystore -alias Mykey -keyalg RSA -keysize 2048 -validity 10000 \
     -dname "CN=TestGuy, OU=MyCompany, O=MyGame, L=MyCity, ST=NC, C=00" \
     -storepass 123456 -keypass 123456
+else
+    keytool \
+        -genkey -v -keystore key.keystore -alias Mykey -keyalg RSA -keysize 2048 -validity 10000 \
+        -dname "CN=TestGuy, OU=MyCompany, O=MyGame, L=MyCity, ST=NC, C=00" \
+        -storepass 123456 -keypass 123456
+fi
 mkdir -p Build/Android
 mv key.keystore Build/Android/
