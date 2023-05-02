3e1ecc2b5519396e3bf2f52dab45fdc84bed3a29
Damon Chan <92778185+damoncro@users.noreply.github.com>
2023-05-02 11:52:36 +0800

    Problem: Missing Erc20Tranfer showcase on metaverse demo (fix cronos-labs/play-unreal-plugin#306) (#44)

---
+++ Metaverse/Source/Metaverse/Public/NPC/Erc20Transfer.h
@@ -0,0 +1,31 @@
+// Copyright 2022, Cronos Labs. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NPC/Help.h"
+#include "Erc20Transfer.generated.h"
+
+struct FWalletSendTXEip155Result;
+
+/**
+ *
+ */
+UCLASS()
+class METAVERSE_API AErc20Transfer : public AHelp {
+	GENERATED_BODY()
+
+  protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnErc20TransferBeginOverlap(
+		UPrimitiveComponent *OverlappedComponent, AActor *OtherActor,
+		UPrimitiveComponent *OtherComp, int32 OtherBodyIndex,
+		bool bFromSweep, const FHitResult &SweepResult);
+
+  private:
+	UFUNCTION()
+        void OnWalletconnectErc20TranferFinished(FWalletSendTXEip155Result TxResult);
+};
